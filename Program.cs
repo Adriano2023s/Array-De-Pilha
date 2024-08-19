f3944b887614668345cdb78e9fc043fba94aa840
Adriano Sousa <adrianodesousa1998@gmail.com>
2024-08-18 22:28:51 -0300

    Criado o primeiro método da atividade.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Vetor_De_Pilha
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nomes = new string[10];
+
+
+        }
+
+        public static void Push(string nome, string[] ListaNomes)
+        {
+            if (nome == null)
+            {
+
+            }
+        }
+    }
+}
